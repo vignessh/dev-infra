f40a017c326b03269531f97d2eb41bde50572439
Vignessh <vignessh@gmail.com>
2018-05-21 14:56:08 +0530

    Upgrade Centos box to newer version; Extra customization for Emacs

--- Vagrantfile
+++ Vagrantfile
@@ -38,8 +38,8 @@ Vagrant.configure("2") do |config|
   # within the machine from a port on the host machine. In the example below,
   # accessing "localhost:8080" will access port 80 on the guest machine.
   # NOTE: This will enable public access to the opened port
-  config.vm.network "forwarded_port", guest: 80, host: 8080
-  config.vm.network "forwarded_port", guest: 8080, host: 28080
+  #config.vm.network "forwarded_port", guest: 80, host: 8080
+  #config.vm.network "forwarded_port", guest: 8080, host: 28080
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine and only allow access
@@ -66,16 +66,15 @@ Vagrant.configure("2") do |config|
   # backing providers for Vagrant. These expose provider-specific options.
   # Example for VirtualBox:
   #
-  config.vm.provider "virtualbox" do |vb|
+  config.vm.provider "virtualbox" do |vb, override|
   #   # Display the VirtualBox GUI when booting the machine
   #   vb.gui = true
   #
     # Customize the amount of memory on the VM:
-    vb.memory = "1024"
+    vb.memory = "2048"
     vb.cpus = 2
     vb.name = "devbox"
-    vb.customize ["modifyvm", "devbox", "--chipset", "ICH9"]
-    vb.customize ["storagectl", "devbox", "--name", "IDE", "--controller", "ICH6"]
+    override.vm.box_url = "https://cloud.centos.org/centos/7/vagrant/x86_64/images/CentOS-7-x86_64-Vagrant-1804_02.VirtualBox.box"
   end
 
   # View the documentation for the provider you are using for more
@@ -99,7 +98,7 @@ Vagrant.configure("2") do |config|
   config.vm.provision "ansible_local", preserve_order: true do |ansible|
     ansible.extra_vars = facts
     ansible.playbook = "provision/main.yml"
-    ansible.install_mode = :pip
+    ansible.install_mode = :default
     # Uncomment the line below in case of provisioning errors to understand the problem better.
     # ansible.verbose = "vvv"
   end
--- provision/roles/development/files/init.el
+++ provision/roles/development/files/init.el
@@ -27,13 +27,9 @@ and a list of packages and create an associated list:
 	(my-packages-alist "melpa-stable"
 			   '(clojure-mode cider))))
 
-(defvar my-packages '(
-                      ;this has to be looked at as the literate starter kit server the purpose of below 3
-                      literate-starter-kit
-                      color-theme
+(defvar my-packages '(color-theme
                       textmate
                       expand-region
-                      starter-kit-eshell
                       clojure-mode
                       cider
 											helm
@@ -44,19 +40,142 @@ and a list of packages and create an associated list:
 											zenburn-theme
 											json-mode
 											json-reformat
-											smartparens)
+											smartparens
+											monokai-theme
+											org
+											adaptive-wrap)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
 
+;; Line numbers
+(global-linum-mode t)
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Use y/n instead of full yes/no for confirmation messages
+(fset 'yes-or-no-p 'y-or-n-p)
 
 (global-company-mode)
 (global-auto-revert-mode 1)
 
-(load-theme 'zenburn t)
+(require 'org)
+
+;; Remove the markup characters, i.e., "/text/" becomes (italized) "text"
+(setq org-hide-emphasis-markers t)
+
+;; Turn on visual-line-mode for Org-mode only
+;; Also install "adaptive-wrap" from elpa
+(add-hook 'org-mode-hook 'turn-on-visual-line-mode)
+
+;; Configure org-mode with Cider
+
+;; Configure Org-mode supported languages
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((clojure . t)
+   (sh . t)
+   (dot . t)
+   (emacs-lisp . t)))
+
+;; Use cider as the Clojure execution backend
+(setq org-babel-clojure-backend 'cider)
+(require 'cider)
+
+;; Useful keybindings when using Clojure from Org
+(org-defkey org-mode-map "\C-x\C-e" 'cider-eval-last-sexp)
+(org-defkey org-mode-map "\C-c\C-d" 'cider-doc)
+
+;; Let's have pretty source code blocks
+(setq org-edit-src-content-indentation 0
+      org-src-tab-acts-natively t
+      org-src-fontify-natively t
+      org-confirm-babel-evaluate nil
+      org-support-shift-select 'always)
+
+			;; Default theme
+			(load-theme 'monokai t)
+
+			;;
+			;; Update the color of the company-mode context menu to fit the Monokai theme
+			;; @source: https://github.com/search?q=deftheme+company-tooltip&type=Code
+			;;
+			(deftheme monokai-overrides)
+
+			(let ((class '((class color) (min-colors 257)))
+			      (terminal-class '((class color) (min-colors 89))))
+
+			  (custom-theme-set-faces
+			   'monokai-overrides
+
+			   ;; Linum and mode-line improvements (only in sRGB).
+			   `(linum
+			     ((,class :foreground "#75715E"
+			              :background "#49483E")))
+			   `(mode-line-inactive
+			     ((,class (:box (:line-width 1 :color "#2c2d26" :style nil)
+			                    :background "#2c2d26"))))
+
+			   ;; Custom region colouring.
+			   `(region
+			     ((,class :foreground "#75715E"
+			              :background "#49483E")
+			      (,terminal-class :foreground "#1B1E1C"
+			                       :background "#8B8878")))
+
+			   ;; Additional modes
+			   ;; Company tweaks.
+			   `(company-tooltip-common
+			     ((t :foreground "#F8F8F0"
+			         :background "#474747"
+			         :underline t)))
+
+			   `(company-template-field
+			     ((t :inherit company-tooltip
+			         :foreground "#C2A1FF")))
+
+			   `(company-tooltip-selection
+			     ((t :background "#349B8D"
+			         :foreground "#BBF7EF")))
+
+			   `(company-tooltip-common-selection
+			     ((t :foreground "#F8F8F0"
+			         :background "#474747"
+			         :underline t)))
+
+			   `(company-scrollbar-fg
+			     ((t :background "#BBF7EF")))
+
+			   `(company-tooltip-annotation
+			     ((t :inherit company-tooltip
+			         :foreground "#C2A1FF")))
+
+			   ;; Popup menu tweaks.
+			   `(popup-menu-face
+			     ((t :foreground "#A1EFE4"
+			         :background "#49483E")))
+
+			   `(popup-menu-selection-face
+			     ((t :background "#349B8D"
+			         :foreground "#BBF7EF")))
+
+			   ;; Circe
+			   `(circe-prompt-face
+			     ((t (:foreground "#C2A1FF" :weight bold))))
+
+			   `(circe-server-face
+			     ((t (:foreground "#75715E"))))
+
+			   `(circe-highlight-nick-face
+			     ((t (:foreground "#AE81FF" :weight bold))))
+
+			   `(circe-my-message-face
+			     ((t (:foreground "#E6DB74"))))
+
+			   `(circe-originator-face
+			     ((t (:weight bold))))))
 
 ;; Cljr customizations
 (require 'clj-refactor)
@@ -66,16 +185,16 @@ and a list of packages and create an associated list:
     (yas-minor-mode 1))
 
 ;; Clojure mode customizations
-(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'clojure-mode-hook #'subword-mode)
 (add-hook 'clojure-mode-hook #'smartparens-mode)
-(add-hook 'clojure-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'clojure-mode-hook #'aggressive-indent-mode)
 (add-hook 'clojure-mode-hook #'cljr-mode)
 
-;; activate helm mode
-(require 'helm-config)
-(helm-mode 1)
+;; Rainbow delimiters
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+;; Show parenthesis mode
+(show-paren-mode 1)
 
 ;; Disable the Emacs menu bar
 (menu-bar-mode -1)
